297813c1151972fda09491701ada9c4779d1767b
Dent Earl <dearl@soe.ucsc.edu>
2011-06-22 16:45:01 -0700

    making all of the external code have correct import statements and adding license information to everything

--- external/evolver_gff_fixgeneix.py
+++ external/evolver_gff_fixgeneix.py
@@ -1,10 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Copyright (C) 2008-2011 by
+# George Asimenos, Robert C. Edgar, Serafim Batzoglou and Arend Sidow.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+##############################
 # Re-assign gene_index
 # Designed for interchr evolver where each chr has gene_index's
 # starting from 0 but ice needs unique.
-
 import sys
-import evolver_gff # -dae, modification for simCtrl
+import evolverSimControl.lib.evolver_gff # -dae, modification for evolverSimCtrl
 import re
 
 def Die(s):
